ed677f313fdf0eac49a1d65ee4beb5fabe7d9b83
Sarit <saritrungj27@gmail.com>
2024-07-20 11:17:52 +0700

    Learn basic of angular

--- learnAngularApp/src/app/app.component.ts
+++ learnAngularApp/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from './components/navbar/Navbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
