25a672da56c7116b213a446f2a193277b8d75687
Sarit <saritrungj27@gmail.com>
2024-07-22 12:57:18 +0700

    Learn basic of angular

--- learnAngularApp/src/app/app.component.ts
+++ learnAngularApp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppNavbar } from './components/navbar/Navbar.component';
+import { AppNavbar } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
@@ -8,8 +8,26 @@ import { HeaderComponent } from './components/header/header.component';
   standalone: true,
   imports: [RouterOutlet, AppNavbar, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'learnAngularApp';
+  title: string = 'this loaded dynamically';
+  imgUrl: string =
+    'https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1200,h_630/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/tsah7c9evnal289z5fig/%E0%B8%9A%E0%B8%B1%E0%B8%95%E0%B8%A3%E0%B9%80%E0%B8%82%E0%B9%89%E0%B8%B2%E0%B8%AA%E0%B8%A7%E0%B8%99%E0%B8%AA%E0%B8%99%E0%B8%B8%E0%B8%81%E0%B9%84%E0%B8%AD%E0%B9%80%E0%B8%AD%E0%B9%87%E0%B8%A1%E0%B8%88%E0%B8%B5%20%E0%B9%80%E0%B8%A7%E0%B8%B4%E0%B8%A5%E0%B8%94%E0%B9%8C%20%E0%B8%AD%E0%B8%AD%E0%B8%9F%20%E0%B9%81%E0%B8%AD%E0%B8%94%E0%B9%80%E0%B8%A7%E0%B8%99%E0%B9%80%E0%B8%88%E0%B8%AD%E0%B8%A3%E0%B9%8C%20(IMG%20Worlds%20of%20Adventure)%20%E0%B9%83%E0%B8%99%E0%B8%94%E0%B8%B9%E0%B9%84%E0%B8%9A.jpg';
+  isDisabled: boolean = true;
+  isActive: boolean = true;
+  fruitName: string = 'Orange';
+
+  buttonclick() {
+    console.log('clicked');
+  }
+
+  keyEnter(event: any) {
+    console.log(event.keyCode);
+    event.keyCode == 13 ? console.log('Enter Key Pressed') : ''
+  }
+
+  keyupFiltering() {
+    console.log('Key Filtering')
+  }
 }
